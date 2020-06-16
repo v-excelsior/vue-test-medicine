06d4447d4d0dba2bcaba2bc3576df1a138aa26e8
vakyla98 <vakyla98@ukr.net>
2020-06-16 21:00:23 +0300

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-import Home from "../views/Home.vue";
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/main-page",
-    name: "MainPage",
-    component: () => import("../views/MainPage.vue"),
-  },
-  {
-    path: "/results",
-    name: "FinishPage",
-    component: () => import("../views/ResultsPage.vue"),
-  },
-];
-const router = new VueRouter({ routes });
-export default router;
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/main-page',
+        name: 'MainPage',
+        component: () => import('../views/MainPage.vue'),
+    },
+    {
+        path: '/results',
+        name: 'FinishPage',
+        component: () => import('../views/ResultsPage.vue'),
+    },
+]
+const router = new VueRouter({ routes })
+export default router
